4da7d5885b4ee58105ad4971cd6889279a1c3950
asmita <asmitakatwal112@gmail.com>
2025-11-11 11:32:54 +0545

    task1

--- task1/obj/Debug/net9.0/task1.AssemblyInfo.cs
+++ task1/obj/Debug/net9.0/task1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1588e987c7058097d01e0de7c41d30a8ec55246")]
 [assembly: System.Reflection.AssemblyProductAttribute("task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
